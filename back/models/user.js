652d123f71354c828d4527734f2d1e3e3b770c5b
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-03-02 16:47:12 +0100

    update the user Schema

--- back/models/user.js
+++ back/models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  email: String,
-  password : String,
-  last_name : String,
-  first_name : String
+  email: {
+    String,
+    lowercase: true,
+    required: true,
+    unique: true
+  },
+  password: { String, required: true , minLength: 6 , maxLength: 14 },
+  last_name: { String, required: true },
+  first_name: { String, required: true },
 });
-
-
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
