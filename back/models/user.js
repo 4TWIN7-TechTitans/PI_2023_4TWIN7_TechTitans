ce43d81f76c711b172c5100148de9b66b53c684a
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-03-02 17:19:17 +0100

    update schema again & added npm install validators

--- back/models/user.js
+++ back/models/user.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
 const userSchema = new mongoose.Schema({
   email: {
     String,
     lowercase: true,
-    required: true,
-    unique: true
+    required: [true, "Please enter a valid email"],
+    unique: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
-  password: { String, required: true , minLength: 6 , maxLength: 14 },
+  password: {
+    String,
+    required: [true, "Please enter a Password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+    maxlength: [14, "Maximum password length is 6 characters"],
+  },
   last_name: { String, required: true },
   first_name: { String, required: true },
 });
