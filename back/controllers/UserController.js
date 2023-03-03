9c1a8f5ec20471541eca552207833eb110dffc03
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-03-03 13:50:36 +0100

    Added Bcrypt to protect Password

--- back/controllers/UserController.js
+++ back/controllers/UserController.js
@@ -3,7 +3,7 @@ const userModel = require("../models/user");
 // handle errors
 const handleErrors = (err) => {
   console.log(err.message, err.code);
-  let errors = { email: "", password: "" };
+  let errors = { email: "", password: "" , last_name :"" , first_name : ""};
 
   // duplicate email error
   if (err.code === 11000) {
@@ -13,10 +13,7 @@ const handleErrors = (err) => {
 
   // validation errors
   if (err.message.includes("user validation failed")) {
-    // console.log(err);
-    Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
+    Object.values(err.errors).forEach(({ properties }) => {      
       errors[properties.path] = properties.message;
     });
   }
@@ -28,9 +25,11 @@ module.exports.signup_post = async (req, res) => {
   const { email, password, last_name, first_name } = req.body;
   try {
     const user = await userModel.create({ email, password, last_name, first_name });
-    res.status(201).json(user);
+    console.log(user);
+    res.status(201).json("User Created , Success");
   } catch (err) {
     const errors = handleErrors(err);
+    console.log({errors});
     res.status(400).json({ errors });
   }
 };
--- back/models/user.js
+++ back/models/user.js
@@ -1,22 +1,35 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    lowercase: true,
-    required: [true, "Please enter a valid email"],
+    required: [true, 'Please enter an email'],
     unique: true,
-    validate: [isEmail, "Please enter a valid email"],
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: [true, "Please enter a Password"],
-    minlength: [6, "Minimum password length is 6 characters"],
-    maxlength: [14, "Maximum password length is 14 characters"],
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   },
   last_name: { type: String, required: true },
   first_name: { type: String, required: true },
+});
+
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+  next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
