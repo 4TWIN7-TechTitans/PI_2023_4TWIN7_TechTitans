cf026def53fde7829478b703915c3cc2bb049cf5
Nacib Mariem <mariem.nacib@esprit.tn>
2023-03-09 08:42:50 +0100

    home index page and notFound page ( with routes )

--- react-back/src/App.js
+++ react-back/src/App.js
@@ -1,22 +1,30 @@
 
 import './App.css';
-import Signup from './userComponent/pages-register';
-import Login from './userComponent/login';
-import { Route, Routes ,  BrowserRouter as Router} from 'react-router-dom';
+// import Signup from './userComponent/pages-register';
+// import Login from './userComponent/login';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+const Signup = lazy(() => import('./userComponent/pages-register'));
+const Login = lazy(() => import('./userComponent/login'));
+const NotFound = lazy(() => import('./userComponent/notFound'));
+const Home = lazy(() => import('./userComponent/index'));
 
 function App() {
   return (
     <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
       <Router>
-      <Routes>
+        <Routes>
           <Route path='/signup' element={<Signup />}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-      
-      </Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='*' element={<NotFound />} />
+          <Route path='/index' element={<Home />} />
+        </Routes>
       </Router>
+    </Suspense>
       {/* <Signup/>
       <Login/> */}
-    </div>
+    </div >
   );
 }
 
