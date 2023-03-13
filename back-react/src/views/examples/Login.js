da5ca00b8cecc1959b5da52520c4ad7444e856fb
Nacib Mariem <mariem.nacib@esprit.tn>
2023-03-13 14:16:51 +0100

    template react back with signup and signin integrated

---
+++ back-react/src/views/examples/Login.js
@@ -0,0 +1,304 @@
+/*!
+=========================================================
+* Argon Dashboard React - v1.2.2
+=========================================================
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col
+} from "reactstrap";
+import React, { useState } from "react";
+import axios from "axios";
+
+
+
+function Login() {
+  const [showNotification, setShowNotification] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [showVerifiedError, setShowVerifiedError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [show2FAform, setShow2FAform] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    if (form.tfa !== undefined) {
+      const code = form.tfa.value;
+
+      await axios
+        .post("http://127.0.0.1:5000/2fa", {
+          email,
+          code,
+        })
+        .then(
+          (res) => {
+            console.log("res 2FA");
+          },
+          (err) => {
+            console.log("err 2FA");
+          }
+        );
+    } else {
+      await axios
+        .post("http://127.0.0.1:5000/login", {
+          email,
+          password,
+        })
+        .then(
+          (res) => {
+            setShowError(false);
+            setShowVerifiedError(false);
+            setShowNotification(true);
+            window.location.href = "/admin/index";
+          },
+          (err) => {
+            console.log("err then");
+            console.log(err.response.data.errors.email);
+
+            if (err.response.data.errors.tfa === "check your sms to 2FA auth") {
+              setShow2FAform(true);
+              setShowVerifiedError(false);
+              setShowError(false);
+            }
+
+            if (err.response.data.errors.email === "email not verified") {
+              setShowVerifiedError(true);
+              setShowError(false);
+            } else {
+              setShowError(true);
+              setShowVerifiedError(false);
+            }
+          }
+        )
+        .catch((err) => {
+          console.log("catch");
+          console.log(err);
+          setShowError(true);
+        });
+    }
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    const emailError = document.querySelector(".email.error");
+
+    let errorMessage = "";
+    if (!validateEmail(email)) {
+      errorMessage +=
+        "&#10060; <span class='error-text'>Please enter a valid email address.</span> ";
+    } else {
+      errorMessage +=
+        "&#9989; <span class='success-text'>Email address is valid.</span> ";
+    }
+
+    emailError.innerHTML = errorMessage;
+  };
+
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    const passwordError = document.querySelector(".password.error");
+    if (!validatePassword(password)) {
+      passwordError.textContent =
+        "Password must be at least 8 characters long.";
+    } else {
+      passwordError.textContent = "";
+    }
+  };
+  return (
+    <>
+      <Col lg="5" md="7">
+        <Card className="bg-secondary shadow border-0">
+          <CardHeader className="bg-transparent pb-5">
+            <div className="text-muted text-center mt-2 mb-3">
+              <small>Sign in with</small>
+            </div>
+            <div className="btn-wrapper text-center">
+              <Button
+                className="btn-neutral btn-icon"
+                color="default"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="btn-inner--icon">
+                  <img
+                    alt="..."
+                    src={
+                      require("../../assets/img/icons/common/github.svg")
+                        .default
+                    }
+                  />
+                </span>
+                <span className="btn-inner--text">Github</span>
+              </Button>
+              <Button
+                className="btn-neutral btn-icon"
+                color="default"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="btn-inner--icon">
+                  <img
+                    alt="..."
+                    src={
+                      require("../../assets/img/icons/common/google.svg")
+                        .default
+                    }
+                  />
+                </span>
+                <span className="btn-inner--text">Google</span>
+              </Button>
+            </div>
+          </CardHeader>
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-center text-muted mb-4">
+              <small>Or sign in with credentials</small>
+            </div>
+            <Form role="form" onSubmit={handleSubmit} noValidate>
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-email-83" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    autoComplete="new-email"
+                    required
+                    onChange={handleEmailChange}
+                  />
+                  
+                </InputGroup>
+                <div className="email error"></div>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Password"
+                    type={showPassword? 'text': 'password'}
+                    autoComplete="new-password"
+                    required
+                    onChange={handlePasswordChange}
+                  />
+                  
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? <i className="fas fa-eye-slash" /> : <i className="fas fa-eye" />}
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+                <div className="password error"></div>
+              </FormGroup>
+
+              {show2FAform && (
+                <>
+                  <label className="form-label" htmlFor="tfa">
+                    2FA Code
+                  </label>{" "}
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="tfa"
+                    required
+                    maxLength={6}
+                  />
+                </>
+              )}
+              <div className="custom-control custom-control-alternative custom-checkbox">
+                <input
+                  className="custom-control-input"
+                  id=" customCheckLogin"
+                  type="checkbox"
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor=" customCheckLogin"
+                >
+                  <span className="text-muted">Remember me</span>
+                </label>
+              </div>
+              <div className="text-center">
+                <Button className="my-4" color="primary" type="button">
+                  Sign in
+                </Button>
+              </div>
+              {showNotification && (
+                <div className="col-12 my-3 alert alert-success">
+                  Login successful!
+                </div>
+              )}
+              {showError && (
+                <div className="col-12 my-3 alert alert-danger">
+                  Invalid email or password.
+                </div>
+              )}
+              {showVerifiedError && (
+                <div className="col-12 my-3 alert alert-danger">
+                  Email not verified.
+                </div>
+              )}
+            </Form>
+          </CardBody>
+        </Card>
+        <Row className="mt-3">
+          <Col xs="6">
+            <a
+              className="text-light"
+              href="#pablo"
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Forgot password?</small>
+            </a>
+          </Col>
+          <Col className="text-right" xs="6">
+            <a
+              className="text-light"
+              href="#pablo"
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Create new account</small>
+            </a>
+          </Col>
+        </Row>
+      </Col>
+    </>
+  );
+};
+
+export default Login;
---
+++ back-react/src/views/examples/Register.js
@@ -0,0 +1,355 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col
+} from "reactstrap";
+import { checkEmail } from "../../services/api";
+import axios from "axios";
+import React, { useState } from "react";
+
+function Register() {
+  const [showNotification, setShowNotification] = useState(false);
+  const [showVerifyEmail, setShowVerifyEmail] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [showError, setShowError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // Add state for showing or hiding the password
+  const [showPassword2, setShowPassword2] = useState(false); // Add state for showing or hiding the password
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const password2 = form.password2.value;
+    const last_name = form.last_name.value;
+    const first_name = form.first_name.value;
+    const role = form.role.value;
+
+    // Verify that passwords match
+    if (password !== password2) {
+      setShowNotification(false);
+      setShowVerifyEmail(false);
+      setErrors({ ...errors, password2: "Passwords do not match" });
+      setShowError(true);
+      return;
+    } 
+    try {
+      // Check if email is already in use
+      console.log(email);
+      const checkEmailRes = await checkEmail(email);
+      if (checkEmailRes) {
+        setShowNotification(true);
+        setShowVerifyEmail(false);
+        setShowError(false);
+        setErrors({ ...errors, email: "Email already in use" }); // Display error message
+        return;
+      }
+      console.log("out");
+      // Register user
+      const registerRes = await axios.post(
+        "http://127.0.0.1:5000/signup",
+        {
+          email,
+          password,
+          last_name,
+          first_name,
+          role,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+    // handle response
+    if (registerRes.status === 201) {
+      setShowNotification(true);
+      setShowVerifyEmail(true);
+      setErrors({});
+      setShowError(false);
+    } else {
+      setShowNotification(false);
+      setShowVerifyEmail(false);
+      setErrors({ ...errors, message: "Signup failed" });
+      setShowError(true);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+  const validateEmail = (email) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  
+  };
+  // const validatePassword = (password) => {
+  //   const lowercaseRegex = /[a-z]/;
+  //   const uppercaseRegex = /[A-Z]/;
+  //   const numberRegex = /[0-9]/;
+
+  //   return (
+  //     password.length >= 8 &&
+  //     lowercaseRegex.test(password) &&
+  //     uppercaseRegex.test(password) &&
+  //     numberRegex.test(password)
+  //   );
+  // };
+
+  const validateFirstName = (last_name) => {
+    const first_nameRegex = /^[a-zA-Z]+$/;
+    return first_nameRegex.test(last_name);
+  };
+
+  const validateLastName = (last_name) => {
+    const last_nameRegex = /^[a-zA-Z]+$/;
+    return last_nameRegex.test(last_name);
+  };
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    const emailError = document.querySelector(".email.error");
+
+    let errorMessage = "";
+    if (!validateEmail(email)) {
+      errorMessage +=
+        "&#10060; <span class='error-text'>Please enter a valid email address.</span> ";
+    } else {
+      errorMessage +=
+        "&#9989; <span class='success-text'>Email address is valid.</span> ";
+    }
+
+    emailError.innerHTML = errorMessage;
+  };
+
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    const passwordError = document.querySelector(".password.error");
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const numberRegex = /[0-9]/;
+  
+    let errorMessage = "";
+    let strengthMessage = "";
+    let strength = 0;
+  
+    if (password.length >= 8) {
+      strength += 1;
+      strengthMessage += "✅ <span class='success-text'>Password is at least 8 characters long.</span> ";
+    } else {
+      strengthMessage += "❌ <span class='error-text'>Password must be at least 8 characters long.</span> ";
+    }
+  
+    if (lowercaseRegex.test(password)) {
+      strength += 1;
+      strengthMessage += "✅ <span class='success-text'>Password contains a lowercase letter.</span> ";
+    } else {
+      strengthMessage += "❌ <span class='error-text'>Password must contain a lowercase letter.</span> ";
+    }
+  
+    if (uppercaseRegex.test(password)) {
+      strength += 1;
+      strengthMessage += "✅ <span class='success-text'>Password contains a capital letter.</span> ";
+    } else {
+      strengthMessage += "❌ <span class='error-text'>Password must contain a capital letter.</span> ";
+    }
+  
+    if (numberRegex.test(password)) {
+      strength += 1;
+      strengthMessage += "✅ <span class='success-text'>Password contains a number.</span> ";
+    } else {
+      strengthMessage += "❌ <span class='error-text'>Password must contain a number.</span> ";
+    }
+  
+    if (strength === 4) {
+      strengthMessage += "✅ <span class='success-text'>Password is strong.</span> ";
+    } else if (strength >= 2) {
+      strengthMessage += "😊 <span class='warning-text'>Password is medium.</span> ";
+    } else {
+      strengthMessage += "😔 <span class='error-text'>Password is weak.</span> ";
+    }
+  
+    errorMessage = strengthMessage;
+    passwordError.innerHTML = errorMessage;
+  };
+
+  const handleFirstNameChange = (e) => {
+    const first_name = e.target.value;
+    const first_nameError = document.querySelector(".first_name.error");
+    if (!validateFirstName(first_name)) {
+      first_nameError.textContent =
+        "Please enter a valid first name (letters only).";
+    } else {
+      first_nameError.textContent = "";
+    }
+  };
+
+  const handleLastNameChange = (e) => {
+    const last_name = e.target.value;
+    const last_nameError = document.querySelector(".last_name.error");
+    if (!validateLastName(last_name)) {
+      last_nameError.textContent =
+        "Please enter a valid last name (letters only).";
+    } else {
+      last_nameError.textContent = "";
+    }
+  };
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="8" className="mx-auto">
+            <Card className="card-user">
+              <CardHeader>
+                <h5 className="title">Register</h5>
+              </CardHeader>
+              <CardBody>
+                <Form onSubmit={handleSubmit} noValidate>
+                  <Row>
+                    <Col md="6">
+                      <FormGroup>
+                        <label>First Name</label>
+                        <Input
+                          name="first_name"
+                          type="text"
+                          placeholder="First Name"
+                          required
+                          onChange={handleFirstNameChange}
+                        />
+                        <div className="first_name error"></div>
+                      </FormGroup>
+                    </Col>
+                    <Col md="6">
+                      <FormGroup>
+                        <label>Last Name</label>
+                        <Input
+                          name="last_name"
+                          type="text"
+                          placeholder="Last Name"
+                          required
+                          onChange={handleLastNameChange}
+                        />
+                        <div className="last_name error"></div>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Email</label>
+                        <Input
+                          name="email"
+                          type="email"
+                          placeholder="Email"
+                          required
+                          onChange={handleEmailChange}
+                        />
+                        <div className="email error"></div>
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="6">
+                      <FormGroup>
+                        <label>Password</label>
+                        <InputGroup>
+                          <Input
+                            name="password"
+                            type={showPassword ? "text" : "password"} // Change input type based on showPassword state
+                            placeholder="Password"
+                            required
+                            onChange={handlePasswordChange} 
+                          />
+                          <div className="password error"></div>
+                          <InputGroupAddon addonType="append">
+                            <InputGroupText onClick={() => setShowPassword(!showPassword)}>
+                              {showPassword ? <i className="fas fa-eye-slash" /> : <i className="fas fa-eye" />}
+                            </InputGroupText>
+                          </InputGroupAddon>
+                        </InputGroup>
+                      </FormGroup>
+                    </Col>
+                    <Col md="6">
+                      <FormGroup>
+                        <label>Confirm </label>
+                        <InputGroup>
+                          <Input
+                            name="password2"
+                            type={showPassword2 ? "text" : "password"} // Change input type based on showPassword2 state
+                            placeholder="Confirm Password"
+                            required
+                          />
+                          <div className="password2 error"></div>
+                          <InputGroupAddon addonType="append">
+                            <InputGroupText onClick={() => setShowPassword2(!showPassword2)}>
+                              {showPassword2 ? <i className="fas fa-eye-slash" /> : <i className="fas fa-eye" />} {/* Change icon based on showPassword2 state */}
+                            </InputGroupText>
+                          </InputGroupAddon>
+                        </InputGroup>
+                        {errors.password2 && <p className="text-danger">{errors.password2}</p>}
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Role</label>
+                        <Input name="role" type="select" required>
+                        <option value="Admin">Admin</option>
+                        <option value="Expert">Expert</option>
+                        <option value="Agence">Agence</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Button className="btn-fill" color="primary" type="submit">
+                  Register
+                </Button>
+              </Form>
+              {showNotification && (
+                  <div className="alert alert-success mt-3" role="alert">
+                    {showVerifyEmail
+                      ? "Signup successful! Please check your email to verify your account."
+                      : "An account with that email already exists."}
+                  </div>
+                )}
+                {showError && (
+                  <div className="col-12 my-3 alert alert-danger">
+                    Invalid fields , Please Recheck !
+                  </div>
+                )}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+</>
+);
+}
+export default Register;
