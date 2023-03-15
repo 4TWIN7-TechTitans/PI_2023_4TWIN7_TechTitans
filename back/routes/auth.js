f8607d3bfc354978aacf514e41e6bffdce5c7076
Oussema-mzoughi <oussema.mzoughi@esprit.tn>
2023-03-15 23:10:07 +0100

    passport and routes

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -71,10 +71,7 @@ router.get(
     const token = createToken(req.user._id);
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
 
-    res.status(200).json({
-      message: "/admin/index",
-      status: "success",
-    });
+    res.redirect('http://localhost:3000/admin/index');
   }
 );
 
