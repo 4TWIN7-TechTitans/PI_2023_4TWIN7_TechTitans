59083573d6934051eaa5167bb0cf29b38cfefead
Oussema-mzoughi <oussema.mzoughi@esprit.tn>
2023-03-15 18:03:30 +0100

    JWT token creation and decoding

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,4 +1,6 @@
 var express = require("express");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
 var router = express.Router();
 const passport = require("passport");
 
@@ -14,9 +16,28 @@ router.get("/google", passport.authenticate("google", { scope: [  'https://www.g
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
-  (req, res) => {
+  async (req, res) => {
+
+    const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: maxAge,
+  });
+};
+    const token = createToken(req.user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    // find user by id and verificationToken
+    const user = await userModel.findOne({
+      _id: decodedToken.id,
+      verificationToken: token,
+    });
+
     res.status(200).json({
-        message: "facebook login OK",
+        link: "admin/index",
         status: "success",
       });
   }
