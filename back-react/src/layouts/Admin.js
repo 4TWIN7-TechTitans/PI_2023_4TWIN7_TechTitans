dd73a9ab18ad589abd0642bbe2483289d8d76ff8
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-03-15 17:24:23 +0100

    Update Logo & Fix some errors

--- back-react/src/layouts/Admin.js
+++ back-react/src/layouts/Admin.js
@@ -1,24 +1,7 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // reactstrap components
-import { Container } from "reactstrap";
+import { Button, Container } from "reactstrap";
 // core components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import AdminFooter from "components/Footers/AdminFooter.js";
@@ -29,6 +12,11 @@ import routes from "routes.js";
 const Admin = (props) => {
   const mainContent = React.useRef(null);
   const location = useLocation();
+ 
+ 
+ 
+
+
 
   React.useEffect(() => {
     document.documentElement.scrollTop = 0;
@@ -75,17 +63,22 @@ const Admin = (props) => {
           imgAlt: "..."
         }}
       />
+      
       <div className="main-content" ref={mainContent}>
         <AdminNavbar
           {...props}
           brandText={getBrandText(props.location.pathname)}
         />
+       
         <Switch>
           {getRoutes(routes)}
+          
           <Redirect from="*" to="/admin/index" />
         </Switch>
+        
         <Container fluid>
           <AdminFooter />
+          
         </Container>
       </div>
     </>
