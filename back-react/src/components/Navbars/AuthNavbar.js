57ba70908b8a873a8ec3d81b217e961f8e55e009
Oussema-mzoughi <oussema.mzoughi@esprit.tn>
2023-03-31 22:06:08 +0100

    update authnavbar

--- back-react/src/components/Navbars/AuthNavbar.js
+++ back-react/src/components/Navbars/AuthNavbar.js
@@ -63,12 +63,7 @@ const AdminNavbar = () => {
               </Row>
             </div>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink className="nav-link-icon" to="/" tag={Link}>
-                  <i className="ni ni-planet" />
-                  <span className="nav-link-inner--text">Dashboard</span>
-                </NavLink>
-              </NavItem>
+             
               <NavItem>
                 <NavLink
                   className="nav-link-icon"
@@ -76,13 +71,13 @@ const AdminNavbar = () => {
                   tag={Link}
                 >
                   <i className="ni ni-circle-08" />
-                  <span className="nav-link-inner--text">Register</span>
+                  <span className="nav-link-inner--text">Sign up</span>
                 </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink className="nav-link-icon" to="/auth/login" tag={Link}>
                   <i className="ni ni-key-25" />
-                  <span className="nav-link-inner--text">Login</span>
+                  <span className="nav-link-inner--text">Sign in</span>
                 </NavLink>
               </NavItem>
 
