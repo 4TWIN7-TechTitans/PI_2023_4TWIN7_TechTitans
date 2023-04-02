67070f739cad3a9901fb92659f7790cf6c4ffcf5
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-04-02 14:41:06 +0100

    OrderMissionExpert

--- back-react/src/views/examples/OrdreMissionExpert.js
+++ back-react/src/views/examples/OrdreMissionExpert.js
@@ -20,32 +20,33 @@ import {
   function OrdreMissionExpert() {
     const [statements, setStatements] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [notification, setNotification] = useState("");
-    const [selectedExpert, setSelectedExpert] = useState("");
-  
-    function getCookie(key) {
-      var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
-      return b ? b.pop() : "";
-    }
-  
-    const fetchData = async (expertEmail) => {
-      try {
-        const response = await axios.get(
-          (`http://127.0.0.1:5000/statementbyexpertemail/` + expertEmail)
-        );
-        const statements = response.data.statements;
-        setStatements(statements);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    const expertEmail = getCookie("email");
-    fetchData(expertEmail);
+    const [expertEmail, setExpertEmail] = useState("");
   
     useEffect(() => {
-      console.log(statements);
-    }, [statements]);
+      function getCookie(key) {
+        var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+        return b ? b.pop() : "";
+      }
+  
+      const email = getCookie("email");
+      setExpertEmail(email);
+  
+      async function fetchData(email) {
+        try {
+          const response = await axios.get(
+            (`http://localhost:5000/statementbyexpertemail/`+email)
+          );
+          const statements = response.data.statements;
+          setStatements(statements);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+  
+      if (email) {
+        fetchData(email);
+      }
+    }, []);
   
     const pageSize = 5;
     const pageCount = Math.ceil(statements.length / pageSize);
@@ -60,50 +61,40 @@ import {
       currentPage * pageSize
     );
   
-    
-
-      return (
-        <>
-          <Header />
-          {/* Page content */}
-          <Container className="mt--7" fluid>
-            <Row>
-              <div className="col">
-                <Card className="shadow">
-                  <CardHeader className="border-0">
-                    <h3 className="mb-0">My Missions</h3>
-                  </CardHeader>
-                  <Table className="align-items-center table-flush" responsive>
-                    <thead className="thead-light">
-                      <tr></tr>
-                    </thead>
-                    <tbody>
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+          <Row>
+            <div className="col">
+              <Card className="shadow">
+                <CardHeader className="border-0">
+                  <h3 className="mb-0">My Missions</h3>
+                </CardHeader>
+                <Table className="align-items-center table-flush" responsive>
+                  <thead className="thead-light">
+                    <tr>
                       <th scope="col">Date</th>
-                      <th scope="col">ContractNumber</th>
-                      <th scope="col">Etat</th>
-                    </tbody>
+                      <th scope="col">Contract Number</th>
+                      <th scope="col">State</th>
+                    </tr>
+                  </thead>
+                  <tbody>
                     {paginatedStatements.map((statement) => {
                       console.log(statement); // Add this line to log the statements object
-
+  
                       return (
                         <tr key={statement._id}>
                           <td>{statement.date}</td>
                           <td>{statement.vehicule_a.contractNumber}</td>
-                          <td>
-                          </td>
-                          <td>
-                          <div className="d-flex">
-                   
-                           
-                             
-                          </div>
-                        </td>
+                          <td>{statement.etat}</td>
                         </tr>
-    
                       );
                     })}
-    
-                  </Table>
+                  </tbody>
+                </Table>
+  
            
     
                   <CardFooter className="py-4">
