ed861c71c434992168e16727908860eee3847fba
Nacib Mariem <mariem.nacib@esprit.tn>
2023-04-15 02:34:15 +0200

    image avatar pour expert et agence

--- back/controllers/UserController.js
+++ back/controllers/UserController.js
@@ -308,11 +308,11 @@ const selectRandomAvatar = async () => {
   const { resources } = await cloudinary.search
     .expression('folder:avatars')
     .execute();
-    const randomIndex = Math.floor(Math.random() * resources.length);
-    const avatar = resources[randomIndex];
-  
-    console.log(randomIndex);
-    console.log(avatar);
+  const randomIndex = Math.floor(Math.random() * resources.length);
+  const avatar = resources[randomIndex];
+
+  console.log(randomIndex);
+  console.log(avatar);
 
   return avatar.secure_url;
 };
@@ -381,9 +381,11 @@ module.exports.add_post = async (req, res) => {
   } */
 
   try {
+    const avatar = await selectRandomAvatar();
     const user = await userModel.create({
       ...req.body,
       verified: "true",
+      image: avatar,
       statements_number: 0,
     });
     if (user) {
@@ -548,9 +550,12 @@ module.exports.add_post = async (req, res) => {
 
                                     <p style="margin: 0;">
                                     Dear ${user.first_name} ${user.last_name},<br>
-                                    We're excited to inform you that you have been added to our website, Assurini! Your profile is now live on our platform, and our users can now discover and connect with your agency.
+                                    We're excited to inform you that you have been added to our website, Assurini! Your profile is now live on our platform.
 
-                                    We believe that your expertise and services will be a great addition to our platform, and we're excited to see the value you will bring to our users. We hope this partnership will be beneficial to both of our organizations.</p>
+                                    We believe that your expertise and services will be a great addition to our platform, and we're excited to see the value you will bring to our users. We hope this partnership will be beneficial to both of our organizations.</p> <br>
+                                    you can connect to assurini with this email : ${user.email} <br>
+                                    and your password is : "${user.email}"<br> 
+                                    please d'ont forget to reset your password after connecting !
                                 </td>
                             </tr>
 
@@ -880,11 +885,200 @@ module.exports.forgot_password_post = async (req, res) => {
       to: "mariem.nacib@esprit.tn",
       subject: "Reset your password",
       html: `
-        <h2>Reset your password</h2>
-        <p>Please click on the link below to reset your password:</p>
-        <a href="http://localhost:3000/auth/resetpwd?token=${random}&email=${email}">Reset Password</a>  
+      <!DOCTYPE html>
+      <html>
+      
+      <head>
+          <title></title>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <style type="text/css">
+              @media screen {
+                  @font-face {
+                      font-family: 'Lato';
+                      font-style: normal;
+                      font-weight: 400;
+                  }
+      
+                  @font-face {
+                      font-family: 'Lato';
+                      font-style: normal;
+                      font-weight: 700;
+                  }
+      
+                  @font-face {
+                      font-family: 'Lato';
+                      font-style: italic;
+                      font-weight: 400;
+                  }
+      
+                  @font-face {
+                      font-family: 'Lato';
+                      font-style: italic;
+                      font-weight: 700;
+                  }
+              }
+      
+              /* CLIENT-SPECIFIC STYLES */
+              body,
+              table,
+              td,
+              a {
+                  -webkit-text-size-adjust: 100%;
+                  -ms-text-size-adjust: 100%;
+              }
+      
+              table,
+              td {
+                  mso-table-lspace: 0pt;
+                  mso-table-rspace: 0pt;
+              }
+      
+              img {
+                  -ms-interpolation-mode: bicubic;
+              }
+      
+              /* RESET STYLES */
+              img {
+                  border: 0;
+                  height: auto;
+                  line-height: 100%;
+                  outline: none;
+                  text-decoration: none;
+              }
+      
+              table {
+                  border-collapse: collapse !important;
+              }
+      
+              body {
+                  height: 100% !important;
+                  margin: 0 !important;
+                  padding: 0 !important;
+                  width: 100% !important;
+              }
+      
+              /* iOS BLUE LINKS */
+              a[x-apple-data-detectors] {
+                  color: inherit !important;
+                  text-decoration: none !important;
+                  font-size: inherit !important;
+                  font-family: inherit !important;
+                  font-weight: inherit !important;
+                  line-height: inherit !important;
+              }
+      
+              /* MOBILE STYLES */
+              @media screen and (max-width:600px) {
+                  h1 {
+                      font-size: 32px !important;
+                      line-height: 32px !important;
+                  }
+              }
+      
+              /* ANDROID CENTER FIX */
+              div[style*="margin: 16px 0;"] {
+                  margin: 0 !important;
+              }
+          </style>
+      </head>
+      
+      <body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
+          <!-- HIDDEN PREHEADER TEXT -->
+          <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account.
+          </div>
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+              <!-- LOGO -->
+              <tr>
+                  <td bgcolor="#172b4d" align="center">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                          <tr>
+                              <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
+                          </tr>
+                      </table>
+                  </td>
+              </tr>
+              <tr>
+                  <td bgcolor="#172b4d" align="center" style="padding: 0px 10px 0px 10px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                          <tr>
+                              <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                                  <h1 style="font-size: 48px; font-weight: 400; margin: 2;">New Password!</h1> <img src="cid:logo" width="125" height="120" style="display: block; border: 0px;" />
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+              </tr>
+              <tr>
+                  <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                          <tr>
+                              <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                  <p style="margin: 0;">Please click on the link below to reset your password:</p>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td bgcolor="#ffffff" align="left">
+                                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                      <tr>
+                                          <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
+                                              <table border="0" cellspacing="0" cellpadding="0">
+                                                  <tr>
+                                                      <td align="center" style="border-radius: 3px;" bgcolor="#172b4d"><a href="http://localhost:3000/auth/resetpwd?token=${random}&email=${email}" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #172b4d; display: inline-block;">Reset Password</a></td>
+                                                  </tr>
+                                              </table>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </td>
+                          </tr> <!-- COPY -->
+                          <tr>
+                              <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                  <p style="margin: 0;">Sent By :<br>Assurini Team</p>
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+              </tr>
+              <tr>
+                  <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                          <tr>
+                              <td bgcolor="#F0FFFF" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                  <h2 style="font-size: 20px; font-weight: 400; color: #111111; margin: 0;">Contact us on :</h2>
+                                  <p style="margin: 0;"><a href="#" target="_blank" style="color: #172b4d;">assurini.tunisien0reply@gmail.com </a></p>
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+              </tr>
+              <tr>
+                  <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                          <tr>
+                              <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;"> <br>
+                                  <p style="margin: 0;">If these emails get annoying, please feel free to <a href="#" target="_blank" style="color: #111111; font-weight: 700;">unsubscribe</a>.</p>
+                              </td>
+                          </tr>
+                      </table>
+                  </td>
+              </tr>
+          </table>
+      </body>
+      
+      </html>       
+        
       `,
+      attachments: [
+        {
+          filename: "logo.png",
+          path: "./public/images/logo.png",
+          cid: "logo",
+        },
+      ],
     };
+
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
