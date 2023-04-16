1cbd291d483f03c3aa75c30b20e53449890fb04d
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-04-16 14:09:11 +0100

    PDF for List Of Experts

--- back-react/src/views/examples/ListOfExperts.js
+++ back-react/src/views/examples/ListOfExperts.js
@@ -16,6 +16,8 @@ import Header from "components/Headers/Header.js";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import jsPDF from 'jspdf';
+
 
 function ListOfAgency() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -59,6 +61,51 @@ function ListOfAgency() {
     (currentPage - 1) * pageSize,
     currentPage * pageSize
   );
+  function generatePDF() {
+    const doc = new jsPDF();
+  
+    // Add agency name
+    doc.setFontSize(14);
+    doc.text("Assurini Agency", 105, 30, {align: "center"});
+  
+    // Add title
+    doc.setFontSize(18);
+    doc.text("List Of Experts", 105, 50, {align: "center"});
+  
+    // Add table headers
+    doc.setFontSize(12);
+    doc.text("Identifiant Du L'Expert", 20, 60);
+    doc.text("Name", 80, 60); // Increased x-coordinate of "Name" header
+    doc.text("Email", 130, 60); // Increased x-coordinate of "Email" header
+    doc.text("Contact Number", 180, 60); // Increased x-coordinate of "Contact Number" header
+  
+    // Add table rows
+    let row = 70;
+    Experts.forEach((user, index) => {
+      if (index % 2 === 0) {
+        doc.setFillColor(240, 240, 240); // Light gray color for even rows
+      } else {
+        doc.setFillColor(255, 255, 255); // White color for odd rows
+      }
+      doc.rect(20, row - 5, 170, 10, "F"); // Draw rectangle to fill row with color
+      doc.setTextColor(0, 0, 0); // Set text color to black
+      doc.text(user._id, 20, row);
+      doc.text(user.first_name, 80, row); // Increased x-coordinate of "Name" text
+      doc.text(user.email, 130, row); // Increased x-coordinate of "Email" text
+      doc.text(user.phone_number, 180, row); // Increased x-coordinate of "Contact Number" text
+      row += 10;
+    });
+    
+    // Add generation date
+    doc.setTextColor(128, 128, 128); // Set text color to gray
+    doc.text(`Generated on ${new Date().toLocaleDateString()}`, doc.internal.pageSize.getWidth() / 2, doc.internal.pageSize.getHeight() - 10, {align: "center"}); // Add date of generation
+  
+    doc.save("list_of_experts.pdf");
+  }
+  
+  
+  
+  
 
   return (
     <>
@@ -129,6 +176,8 @@ function ListOfAgency() {
                     </PaginationItem>
                   </Pagination>
                 </nav>
+                <Button onClick={generatePDF}>Downaload List Of Experts</Button>
+
               </CardFooter>
             </Card>
           </div>
