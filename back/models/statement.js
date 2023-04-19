61d720476adb5b2520f71a0d07e9601ff302ce13
Nacib Mariem <mariem.nacib@esprit.tn>
2023-04-20 00:54:08 +0200

    witness_a / witness_b back

--- back/models/statement.js
+++ back/models/statement.js
@@ -27,17 +27,51 @@ const statementSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  //Partie 5
-  witness: {
+    //Partie 5
+  witness_a: [
+    {
+    firstName_w: {
+      type: String,
+      maxlength: [50, "First name should not exceed 50 characters"],
+      required: false,
+    },
+    lastName_w: {
+      type: String,
+      maxlength: [50, "Last name should not exceed 50 characters"],
+      required: false,
+    },
+    addressWitness: {
+      type: String,
+      required: false,
+    },
+    phoneWitness: {
+      type: String,
+      required: false,
+    },
+  }
+  ],
+witness_b: [
+  {
+  firstName_w: {
     type: String,
-
-    maxlength: [50, "Witness name should not exceed 50 characters"],
-    match: [
-      /^[A-Za-z\s'À-ÖØ-öø-ÿ]+$/,
-      "Witness name should only contain letters, spaces, quotes, and French characters",
-    ],
+    maxlength: [50, "First name should not exceed 50 characters"],
     required: false,
   },
+  lastName_w: {
+    type: String,
+    maxlength: [50, "Last name should not exceed 50 characters"],
+    required: false,
+  },
+  addressWitness: {
+    type: String,
+    required: false,
+  },
+  phoneWitness: {
+    type: String,
+    required: false,
+  },
+},],
+
   //Partie 6
   //Vehicule A
   vehicule_a: {
