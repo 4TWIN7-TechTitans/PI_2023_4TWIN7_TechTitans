0b4d85ca8a36c6473b13c072d7634eb548c7c3f2
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-04-24 08:27:57 +0100

    Added Notification For Tickets

--- back-react/src/views/examples/Tickets.js
+++ back-react/src/views/examples/Tickets.js
@@ -173,6 +173,11 @@ const Tickets = () => {
         const filteredData = response.data.tickets;
         setTickets(filteredData);
       }
+      toast.success('Welcome Dear Agency, you are in Our List Tickets !', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 30000, 
+      });
+
     } catch (error) {
       console.log(error);
     }
