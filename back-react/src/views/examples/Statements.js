a1841a791b8f1d22a39741ef242f48673eb3a975
Nacib Mariem <mariem.nacib@esprit.tn>
2023-04-24 09:54:02 +0200

    show witnesses in show details fixed

--- back-react/src/views/examples/Statements.js
+++ back-react/src/views/examples/Statements.js
@@ -43,8 +43,6 @@ function MyStatements() {
     setNom(decodeURI(getCookie("lastname")));
     setPrenom(decodeURI(getCookie("firstname")));
     setRole(decodeURI(getCookie("role")));
-    console.log(role);
-
     fetchData();
   }, [nom, prenom, role]);
 
@@ -58,7 +56,7 @@ function MyStatements() {
           statement.insured_a.lastname === nom
       );
 
-      console.log(filteredData);
+
       setStatements(filteredData);
 
     } catch (error) {
@@ -74,7 +72,6 @@ function MyStatements() {
   }, []);
 
   useEffect(() => {
-    console.log(statements);
   }, [statements]);
 
   const showNotification = (message) => {
@@ -129,7 +126,6 @@ function MyStatements() {
           statement.insured_a.firstname === prenom &&
           statement.insured_a.lastname === nom
       );
-      console.log(filteredData);
       setStatements(filteredData);
     } catch (error) {
       console.log(error);
@@ -210,7 +206,6 @@ function MyStatements() {
 
                 </tbody>
                 {paginatedStatements.map((statement) => {
-                  console.log(statement); // Add this line to log the statements object
                   let statusText = "";
                   let color = "orange";
                   switch (statement.case_state) {
@@ -336,7 +331,7 @@ function MyStatements() {
                 <center><h2>Statement Details</h2></center>
                 <hr />
                 <div style={{ marginBottom: "20px" }}>
-                  <h4 class="font-weight-bold" style={{ color: "#666", marginBottom: "10px" }}>Accident Information</h4>
+                  <h4 className="font-weight-bold" style={{ color: "#666", marginBottom: "10px" }}>Accident Information</h4>
                   <p style={{ marginBottom: "5px" }}>Date: {selectedStatement.date}</p>
                   <p style={{ marginBottom: "5px" }}>Location: {selectedStatement.location}</p>
                   <p style={{ marginBottom: "5px" }}>Injured: {selectedStatement.injured}</p>
@@ -357,7 +352,7 @@ function MyStatements() {
                 <hr />
                 <div>
                   <div style={{ marginBottom: "20px" }}>
-                    <h4 class="font-weight-bold" style={{ color: "#666", marginBottom: "10px" }}>Driver's Identity</h4>
+                    <h4 className="font-weight-bold" style={{ color: "#666", marginBottom: "10px" }}>Driver's Identity</h4>
                     <div style={{ marginBottom: "10px" }}>
                       <h6 style={{ color: "#999", marginBottom: "5px" }}>Driver A:</h6>
                       <p style={{ marginBottom: "5px" }}>First Name: {selectedStatement.drivers_identity_a.first_name}</p>
@@ -368,7 +363,7 @@ function MyStatements() {
                     </div>
                   </div>
                   <div>
-                    <h6 class="font-weight-bold">Driver B:</h6>
+                    <h6 className="font-weight-bold">Driver B:</h6>
                     <p>First Name: {selectedStatement.drivers_identity_b.first_name}</p>
                     <p>Last Name: {selectedStatement.drivers_identity_b.last_name}</p>
                     <p>Address: {selectedStatement.drivers_identity_b.address}</p>
@@ -378,7 +373,7 @@ function MyStatements() {
                 </div>
                 <hr />
                 <div>
-                  <h4 class="font-weight-bold">Insured Information</h4>
+                  <h4 className="font-weight-bold">Insured Information</h4>
                   <div>
                     <h6>Insured A:</h6>
                     <p>First Name: {selectedStatement.insured_a.firstname}</p>
@@ -387,7 +382,7 @@ function MyStatements() {
                     <p>Address: {selectedStatement.insured_a.addr}</p>
                   </div>
                   <div>
-                    <h6 class="font-weight-bold">Insured B:</h6>
+                    <h6 className="font-weight-bold">Insured B:</h6>
                     <p>First Name: {selectedStatement.insured_b.firstname}</p>
                     <p>Last Name: {selectedStatement.insured_b.lastname}</p>
                     <p>Phone Number: {selectedStatement.insured_b.phonenumber}</p>
@@ -396,9 +391,9 @@ function MyStatements() {
                 </div>
                 <hr />
                 <div>
-                  <h4 class="font-weight-bold">Vehicle Information</h4>
+                  <h4 className="font-weight-bold">Vehicle Information</h4>
                   <div>
-                    <h6 class="font-weight-bold">Vehicle A:</h6>
+                    <h6 className="font-weight-bold">Vehicle A:</h6>
                     <p>Brand: {selectedStatement.vehicule_identity_a.brand}</p>
                     <p>Type: {selectedStatement.vehicule_identity_a.type}</p>
                     <p>Matriculation: {selectedStatement.vehicule_identity_a.matriculation}</p>
@@ -407,7 +402,7 @@ function MyStatements() {
                     <p>Going To: {selectedStatement.vehicule_identity_a.going_to}</p>
                   </div>
                   <div>
-                    <h6 class="font-weight-bold">Vehicle B:</h6>
+                    <h6 className="font-weight-bold">Vehicle B:</h6>
                     <p>Brand: {selectedStatement.vehicule_identity_b.brand}</p>
                     <p>Type: {selectedStatement.vehicule_identity_b.type}</p>
                     <p>Matriculation: {selectedStatement.vehicule_identity_b.matriculation}</p>
@@ -416,7 +411,7 @@ function MyStatements() {
                     <p>Going To: {selectedStatement.vehicule_identity_b.going_to}</p>
                   </div>
                   <div>
-                    <h6 class="font-weight-bold">Vehicle Owner A:</h6>
+                    <h6 className="font-weight-bold">Vehicle Owner A:</h6>
                     <p>Agency : {selectedStatement.vehicule_a.assureBy}</p>
                     <p>Contract Number: {selectedStatement.vehicule_a.contractNumber}</p>
                     <p>Agency Branche: {selectedStatement.vehicule_a.agency}</p>
@@ -424,7 +419,7 @@ function MyStatements() {
                     <p>End Date of Contract: {selectedStatement.vehicule_a.contractValidity.end_date}</p>
                   </div>
                   <div>
-                    <h6 class="font-weight-bold">Vehicle Owner B:</h6>
+                    <h6 className="font-weight-bold">Vehicle Owner B:</h6>
                     <p>Agency: {selectedStatement.vehicule_b.assureBy}</p>
                     <p>Contract Number: {selectedStatement.vehicule_b.contractNumber}</p>
                     <p>Agency Branche: {selectedStatement.vehicule_b.agency}</p>
@@ -434,7 +429,7 @@ function MyStatements() {
                 </div>
                 <hr />
                 <div>
-                  <h4 class="font-weight-bold">Additional Information</h4>
+                  <h4 className="font-weight-bold">Additional Information</h4>
                   <p>Choc Points of Client A: {selectedStatement.hits_a}</p>
                   <p>Choc Points of Client B: {selectedStatement.hits_b}</p>
                   <p>Apparent Damages A: {selectedStatement.apparent_damages_a}</p>
