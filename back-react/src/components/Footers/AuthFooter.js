ab5d0fc3a8b26a138bc88a4aeda4f9efa15c3280
Nacib Mariem <mariem.nacib@esprit.tn>
2023-04-28 11:37:32 +0200

    footer

--- back-react/src/components/Footers/AuthFooter.js
+++ back-react/src/components/Footers/AuthFooter.js
@@ -18,150 +18,78 @@
 /*eslint-disable*/
 
 // reactstrap components
-import { NavItem, NavLink, Nav, Container, Row, Col ,Button } from "reactstrap";
-
+import { NavItem, NavLink, Nav, Container, Row, Col, Button } from "reactstrap";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 const Login = () => {
   return (
     <>
 
-<div className="container my-5">
 
-  <section className="">
-  
-  <footer className="text-center text-white" >
-    
-    <div className="container p-4 pb-0 positionInBottom" >
-      
-     
 
-      © 2020 Copyright&nbsp;&nbsp;:
-      <a className="text-white" href="/admin/index"> &nbsp;&nbsp;Assurini</a>
-    </div>
-  </footer>
-</section>
-  {/*
-  <section className="">
-  <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
-        </div>
+         <section className="p-6 pb-0">
+{/* 
+          <footer className="text-center text-white" >
 
-        <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
-      </section>
+            <div className="container p-4 pb-0 positionInBottom" >
 
-      <section className=''>
-         <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                Company name
-              </h6>
-              <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit.
-              </p>
-            </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Angular
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  React
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Vue
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
 
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
-                </a>
-              </p>
-            </MDBCol>
+              © 2020 Copyright&nbsp;&nbsp;:
+              <a className="text-white" href="/admin/index"> &nbsp;&nbsp;Assurini</a>
+            </div>
+          </footer> */}
+        </section> 
+<footer>
 
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon icon="home" className="me-2" />
-                New York, NY 10012, US
+        <MDBFooter className='text-center text-white' style={{ backgroundColor: '#0a4275' }}>
+          <MDBContainer className='p-3 pb-0' >
+            <section className=''>
+              <p className='d-flex justify-content-center align-items-center 8rem'>
+                <span className='me-3' font-aria-setsize={8}><b >Register for free</b>&nbsp; &nbsp; </span>
+                <MDBBtn type='button' outline color="light" rounded>
+                  Sign up!
+                </MDBBtn>
               </p>
-              <p>
-                <MDBIcon icon="envelope" className="me-3" />
-                info@example.com
-              </p>
-              <p>
-                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
-              </p>
-            </MDBCol>
-          </MDBRow></MDBContainer> 
-        
-      </section>
+            </section>
+          </MDBContainer>
+          <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='#!'>
+         <MDBIcon icon="phone" className="me-3" /><p>(+216) 20 760 820  </p>
+          </MDBBtn>
 
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
-    </MDBFooter>
-</section>
-  */}
-</div>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!'>
+          <MDBIcon icon="envelope" className="me-6" /><p>assurini.tunisien0reply@gmail.com</p>
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' >
+          <MDBIcon fab icon='github' /><p>4TWIN7-TechTitans</p>
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!'>
+          <MDBIcon icon='map' className="me-6" /><p>Esprit School of Engineering</p>
+          </MDBBtn>
+
+        </section>
+      </MDBContainer>
+      <div ><iframe width="50%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=50%25&amp;height=300&amp;hl=en&amp;q=+(Esprit%20school%20of%20engineering)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe></div>
+          <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+            © 2023 Copyright :&nbsp; &nbsp; 
+            <a className='text-white' href='https://mdbootstrap.com/'>
+            &nbsp; ASSURINI 
+            </a>
+          </div>
+        </MDBFooter>
+
+        </footer>
     </>
   );
 };
