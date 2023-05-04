3ce0b5e0d2f05bf7c6c1d9082e5b33c806ef9186
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-05-04 19:52:08 +0100

    ChatGpt ---> AssuriniChat

--- back-react/src/views/Index.js
+++ back-react/src/views/Index.js
@@ -11,89 +11,116 @@ import {
 import Header from "components/Headers/Header";
 import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
+import axios from "axios";
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+
+// "Explain things like you would to a 10 year old learning how to code."
+
 
 const Index = () => {
   function getCookie(key) {
     var b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
     return b ? b.pop() : "";
   }
+  const API_KEY = "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";
+  const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+    "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+  }
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm Assurini! Ask me anything!",
+      sentTime: "just now",
+      sender: "Assurini Bot is Typing"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
 
-  const [accidentsCount, setAccidentsCount] = useState({});
-  const [topLocations, setTopLocations] = useState([]);
-
-  useEffect(() => {
-    const fetchStatements = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/getstatements");
-        if (!response.ok) {
-          throw new Error(`HTTP error ${response.status}`);
-        }
-        const data = await response.json();
-        console.log("Data:", data); // Debugging statement to check the format of the data
-        if (!Array.isArray(data)) {
-          throw new Error("Data is not an array");
-        }
-
-        const formattedData = data.data
-          .filter((d) => d.location !== "") // Remove records without a location
-          .map((d) => ({ date: new Date(d.date), location: d.location }));
-
-        let accidentsCount = 0; // Define accidentsCount variable with initial value of 0
-
-        for (let i = 0; i < formattedData.length; i++) {
-          if (formattedData[i].location === "Accident") {
-            accidentsCount++;
-          }
-        }
-
-        const topLocations = Object.entries(accidentsCount)
-          .sort((a, b) => b[1] - a[1])
-          .slice(0, 10);
-
-        setAccidentsCount(accidentsCount);
-        setTopLocations(topLocations);
-      } catch (error) {
-        console.error("Error fetching statements:", error);
-      }
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
     };
 
-    fetchStatements();
-  }, []);
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
 
-  const chartData = {
-    labels: topLocations.map((l) => l[0]),
-    datasets: [
-      {
-        label: "Nombre d'accidents",
-        data: topLocations.map((l) => l[1]),
-        backgroundColor: "rgba(75,192,192,1)",
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 1,
-      },
-    ],
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
   };
 
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatGPT to act. 
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatGPT
+        ...apiMessages // The messages from our chat with ChatGPT
+      ]
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000)); // add a delay of 1 second
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
+  }
   return (
     <>
       <Header />
       <Card className="shadow"></Card>
-
-      <div>
-        <h2>Les 10 emplacements les plus accidentogènes</h2>
-        <Bar
-          data={chartData}
-          options={{
-            title: {
-              display: true,
-              text: "Les 10 emplacements les plus accidentogènes",
-              fontSize: 20,
-            },
-            legend: {
-              display: true,
-            },
-          }}
-        />
+      <div style={{ position:"relative", height: "800px", width: "1672px"  }}>
+      <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="Assurini is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
       </div>
+
     </>
   );
 };
