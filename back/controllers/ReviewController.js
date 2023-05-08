f86abdb15a02ffd2c614886745363c5a90c5dd7f
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-05-09 00:00:52 +0100

    update reviewController
    Co-Authored-By: Mahmoud Cheikh <15525630+MahmoudCheikh@users.noreply.github.com>

--- back/controllers/ReviewController.js
+++ back/controllers/ReviewController.js
@@ -1,29 +1,29 @@
 const ReviewModel = require("../models/review");
 
 
-module.exports.addReview = function(req, res) {
-  const newReview = new ReviewModel({
-    id_expert: req.body.id_expert,
-    review: req.body.review,
-  });
+module.exports.addReview = function (req, res) {
+    const newReview = new ReviewModel({
+        id_expert: req.body.id_expert,
+        review: req.body.review,
+    });
 
-  newReview.save(function(err, review) {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: "Failed to add review" });
-    } else {
-      res.status(201).json({ message: "Review added successfully", review });
-    }
-  });
+    newReview.save(function (err, review) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "Failed to add review" });
+        } else {
+            res.status(201).json({ message: "Review added successfully", review });
+        }
+    });
 };
 
-module.exports.getAllReviews = function(req, res) {
-    ReviewModel.find({}, function(err, reviews) {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: "Failed to retrieve reviews" });
-      } else {
-        res.status(200).json({ reviews });
-      }
+module.exports.getAllReviews = function (req, res) {
+    ReviewModel.find({}, function (err, reviews) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ message: "Failed to retrieve reviews" });
+        } else {
+            res.status(200).json({ reviews });
+        }
     });
-  };
+};
