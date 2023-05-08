005db8fdc56ca319eea1e18acaa78ed55635b52f
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-05-09 00:01:28 +0100

    updateModel
    Co-Authored-By: Mahmoud Cheikh <15525630+MahmoudCheikh@users.noreply.github.com>

--- back/models/review.js
+++ back/models/review.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-  note: {
-    type: String,
-    required: false,
-  },
-  id_expert: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Expert",
-    required: false,
-  },
-  review: {
-    type: String,
-    required: false,
-  },
+    note: {
+        type: String,
+        required: false,
+    },
+    id_expert: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Expert",
+        required: false,
+    },
+    review: {
+        type: String,
+        required: false,
+    },
 });
 
 const Review = mongoose.model("Review", ReviewSchema);
