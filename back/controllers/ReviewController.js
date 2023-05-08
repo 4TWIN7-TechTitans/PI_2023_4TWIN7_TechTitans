5739a22d0c895c72b9d65f4a182634852db1c77b
Mahmoud Cheikh <mahmoud.cheikh.02@gmail.com>
2023-05-09 00:16:42 +0100

    Update ReviewController.js
    Co-Authored-By: Ahmed Elmoez Noomen <60655635+MrAhmedElmoez69@users.noreply.github.com>

--- back/controllers/ReviewController.js
+++ back/controllers/ReviewController.js
@@ -1,26 +1,26 @@
 const ReviewModel = require("../models/review");
 
-
 module.exports.addReview = async (req, res) => {
-    try {
-      const newReview = await ReviewModel.create({
-        id_expert: req.body.id_expert,
-        review: req.body.review,
-      });
-      res.status(201).json({ message: "Review added successfully", review: newReview });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Failed to add review" });
-    }
-  };
-  
-  module.exports.getAllReviews = async (req, res) => {
-    try {
-      const reviews = await ReviewModel.find({});
-      res.status(200).json({ reviews });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Failed to retrieve reviews" });
-    }
-  };
-  
+  try {
+    const newReview = await ReviewModel.create({
+      id_expert: req.body.id_expert,
+      review: req.body.review,
+    });
+    res
+      .status(201)
+      .json({ message: "Review added successfully", review: newReview });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to add review" });
+  }
+};
+
+module.exports.getAllReviews = async (req, res) => {
+  try {
+    const reviews = await ReviewModel.find({});
+    res.status(200).json({ reviews });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve reviews" });
+  }
+};
