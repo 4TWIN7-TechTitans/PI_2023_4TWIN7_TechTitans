e05169ba9ce03105ad813357d2062e2d3999bda0
Mahmoud Cheikh <mahmoud.cheikh.02@gmail.com>
2023-05-09 01:22:52 +0100

    get sent analysis by id review
    Co-Authored-By: Ahmed Elmoez Noomen <60655635+MrAhmedElmoez69@users.noreply.github.com>
    Co-Authored-By: Nacib Mariem <67750731+Nacib1999@users.noreply.github.com>

--- back/controllers/ReviewController.js
+++ back/controllers/ReviewController.js
@@ -24,3 +24,71 @@ module.exports.getAllReviews = async (req, res) => {
     res.status(500).json({ message: "Failed to retrieve reviews" });
   }
 };
+
+const { spawn } = require('child_process');
+module.exports.calc = async (req, res) => {
+  try {
+    const id = req.body.id;
+    console.log(id);
+
+    const review = await ReviewModel.findById(id);
+    // 64598243379f1afd34842690
+    //exec python
+
+    let args = review.review;
+
+    //get result
+
+    const prom = new Promise((resolve, reject) => {
+      const pythonProcess = spawn("python", [
+        "C:/repos/PI_2023_4TWIN7_TechTitans/back/scripts/m/emotion.py",
+        args,
+      ]);
+      let newdata = "";
+      pythonProcess.stdout.on("data", (data) => {
+        newdata += data.toString();
+        // console.log("datadata "  + newdata)
+      });
+
+      pythonProcess.stderr.on("data", (data) => {
+        // console.error(`stderr: ${data}`);
+      });
+
+      pythonProcess.on("close", (code) => {
+        console.log(`child process exited with code ${code}`);
+        if (code === 0) {
+          const sentiment = newdata.trim(); // Remove any leading/trailing whitespace
+            // console.log("sensensnesne " + sentiment)
+          resolve(sentiment);
+        } else {
+        }
+      });
+    });
+
+
+    await prom;
+    let returnresult = "";
+    // prom.then((result) => {
+    //   console.log(result);
+    //   returnresult = result;
+    // });
+
+
+    
+    prom
+    .then((sentiment) => {
+      console.log(`Sentiment: ${sentiment}`);
+      const sub = sentiment.substr(35, 10)
+      res.status(200).json({ sub });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+
+    // res.status(200).json({ returnresult });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to retrieve reviews" });
+  }
+};
