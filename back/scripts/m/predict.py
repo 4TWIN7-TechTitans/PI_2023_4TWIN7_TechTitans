61a6b785594a414a07ca17c42fee1663df0169e0
Mahmoud Cheikh <mahmoud.cheikh.02@gmail.com>
2023-05-10 14:10:11 +0100

    update models

--- back/scripts/m/predict.py
+++ back/scripts/m/predict.py
@@ -16,20 +16,20 @@ from sklearn.metrics import accuracy_score,classification_report,confusion_matri
 from sklearn.model_selection import KFold # import KFold
 import warnings
 import pickle
+import joblib
 warnings.filterwarnings('ignore')
 
 
 from sklearn.preprocessing import OrdinalEncoder
 
-"""Import pickled model"""
+model = joblib.load("C:/repos/PI_2023_4TWIN7_TechTitans/back/scripts/m/model.joblib")
+OE_save = joblib.load("C:/repos/PI_2023_4TWIN7_TechTitans/back/scripts/m/encoder.joblib")
 
-OE_save = pickle.load(open('Oencoder.pkl','rb'))
 
-model=pickle.load(open('model.sav','rb'))
-
+#OE_save = pickle.load(open('Oencoder.pkl','rb'))
+#model=pickle.load(open('model.sav','rb'))
 # load your test dataset here
-df_sample = pd.read_csv("check.csv")
-
+df_sample = pd.read_csv("C:/repos/PI_2023_4TWIN7_TechTitans/back/scripts/m/check2.csv")
 
 
 lists=['Vehicle_driver_relation', 'Work_of_casuality', 'Fitness_of_casuality','Day_of_week','Casualty_severity','Time','Sex_of_driver','Educational_level','Defect_of_vehicle','Owner_of_vehicle','Service_year_of_vehicle', 'Road_surface_type','Sex_of_casualty']
@@ -37,30 +37,31 @@ df_sample.drop(columns = lists, inplace=True)
 
 cols = df_sample.columns
 
+
 # convert numpy array to pandas dataframe
 df_sample_tf = OE_save.transform(df_sample)
 xs = pd.DataFrame(df_sample_tf,columns=cols)
 
-
 test = xs["Accident_severity"]
 dfx = xs.drop(columns=["Accident_severity"])
 
+
 predictions=model.predict(dfx)
 
-accuracy = accuracy_score(test,predictions)
+#accuracy = accuracy_score(test,predictions)
 
-accuracy
+#accuracy
 
 print(predictions)
 
 # add prediction column to df
 # invert the df
-dfx["Accident_severity"] = predictions
-inv_array = OE_save.inverse_transform(dfx)
-inv_df = pd.DataFrame(inv_array,columns=cols)
+#dfx["Accident_severity"] = predictions
+#inv_array = OE_save.inverse_transform(dfx)
+#inv_df = pd.DataFrame(inv_array,columns=cols)
 
-inv_df
+#inv_df
 
 
-print(inv_array)
-print(inv_df)
+#print(inv_array)
+#print(inv_df)
